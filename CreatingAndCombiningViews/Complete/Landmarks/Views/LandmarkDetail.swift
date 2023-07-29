772fc35399b5f9b7f21c93093b0ef2cfbaec0984
Shaw Lu <shawlu95@gmail.com>
2023-07-28 22:41:29 -0700

    set nav title

--- CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkDetail.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkDetail.swift
@@ -56,6 +56,8 @@ struct LandmarkDetail: View {
             }
             .padding()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
