95d873d2d506d727a2246ceec4b2ba208af90a69
Shaw Lu <shawlu95@gmail.com>
2023-07-28 22:23:49 -0700

    create table view with static cell

---
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by main on 7/28/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
