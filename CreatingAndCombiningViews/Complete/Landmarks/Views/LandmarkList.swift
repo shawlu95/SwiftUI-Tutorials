800f156bca0d3cbe605e133c2d951b7d7e11cdee
Shaw Lu <shawlu95@gmail.com>
2023-07-28 22:25:26 -0700

    make dynamic list of views

--- CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkList.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkList.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
+        // Lists work with identifiable data.
+        List(landmarks, id: \.id) { landmark in
+            LandmarkRow(landmark: landmark)
         }
     }
 }
