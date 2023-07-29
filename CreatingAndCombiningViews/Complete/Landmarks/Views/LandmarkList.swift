a4aabbb6856455bd73033d73924f9c5e6c1de4b3
Shaw Lu <shawlu95@gmail.com>
2023-07-28 22:45:18 -0700

    preview on multiple device

--- CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkList.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkList.swift
@@ -26,6 +26,15 @@ struct LandmarkList: View {
 }
 
 #Preview {
-    LandmarkList()
-        .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
+    /*
+     ForEach operates on collections the same way as the list, which
+     means you can use it anywhere you can use a child view, such as
+     in stacks, lists, groups, and more. When the elements of your
+     data are simple value types — like the strings you’re using here
+     — you can use \.self as key path to the identifier.*/
+    ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .previewDisplayName(deviceName)
+    }
 }
