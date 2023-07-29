f9406e862a8cd06c0ba91d9edf4bd190856267cf
Shaw Lu <shawlu95@gmail.com>
2023-07-28 21:48:02 -0700

    create custom row view

---
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by main on 7/28/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    // add as stored property
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    // use the first landmark for preview
+    LandmarkRow(landmark: landmarks[0])
+}
