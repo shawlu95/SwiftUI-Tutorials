91ff6bc41cdf64ba2de3bb22679470c27ef7a0a7
Shaw Lu <shawlu95@gmail.com>
2023-07-28 21:37:09 -0700

    add nested field

--- CreatingAndCombiningViews/Complete/Landmarks/Landmark.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Landmark.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -22,4 +23,17 @@ struct Landmark: Hashable, Codable {
     var image: Image {
         Image(imageName)
     }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
 }
