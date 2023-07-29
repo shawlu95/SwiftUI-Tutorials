db7df3fd48a4bb6d05d10b85accd6a60d5d971d7
Shaw Lu <shawlu95@gmail.com>
2023-07-28 22:37:46 -0700

    refactor to use stored value to render view

--- CreatingAndCombiningViews/Complete/Landmarks/Views/MapView.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/Views/MapView.swift
@@ -9,16 +9,15 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
     /* You use the @State attribute to establish a source
      of truth for data in your app that you can modify from
      more than one view. SwiftUI manages the underlying
      storage and automatically updates views that depend on
      the value.
      */
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
 
     /*
      By prefixing a state variable with $, you pass a
@@ -29,11 +28,22 @@ struct MapView: View {
      */
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(
+            latitude: 34.011_286,
+            longitude: -116.166_868))
     }
 }
