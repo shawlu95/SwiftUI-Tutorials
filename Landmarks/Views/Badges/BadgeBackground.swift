5cf9bf19d7938e122ae8db48fc7d66ec91fa46e0
Shaw Lu <shawlu95@gmail.com>
2023-08-15 02:01:56 +0800

    draw hexagon

---
+++ Landmarks/Views/Badges/BadgeBackground.swift
@@ -0,0 +1,38 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by main on 8/15/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+            
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+            }
+        }
+        .fill(.black)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
