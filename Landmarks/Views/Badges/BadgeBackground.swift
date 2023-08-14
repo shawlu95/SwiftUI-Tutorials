e49b26c40f434a3b9395dadd4009edad1f8e238c
Shaw Lu <shawlu95@gmail.com>
2023-08-15 03:06:17 +0800

    wrap geometry reader

--- Landmarks/Views/Badges/BadgeBackground.swift
+++ Landmarks/Views/Badges/BadgeBackground.swift
@@ -10,37 +10,40 @@ import SwiftUI
 
 struct BadgeBackground: View {
     var body: some View {
-        Path { path in
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(
-                to: CGPoint(
-                    x: width * 0.95,
-                    y: height * 0.20
-                )
-            )
-            
-            HexagonParameters.segments.forEach { segment in
-                path.addLine(
+        // GeometryReader allows badge to use its parent view
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                path.move(
                     to: CGPoint(
-                        x: width * segment.line.x,
-                        y: height * segment.line.y
+                        x: width * 0.95,
+                        y: height * 0.20
                     )
                 )
                 
-                path.addQuadCurve(
-                    to: CGPoint(
-                        x: width * segment.curve.x,
-                        y: height * segment.curve.y
-                    ),
-                    control: CGPoint(
-                        x: width * segment.control.x,
-                        y: height * segment.control.y
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x,
+                            y: height * segment.line.y
+                        )
                     )
-                )
+                    
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
             }
+            .fill(.black)
         }
-        .fill(.black)
     }
 }
 
