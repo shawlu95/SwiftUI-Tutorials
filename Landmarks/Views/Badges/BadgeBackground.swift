c5d2afb761570f74be408f4f8dacc541e547ce07
Shaw Lu <shawlu95@gmail.com>
2023-08-15 03:09:07 +0800

    scale x-axis and recenter

--- Landmarks/Views/Badges/BadgeBackground.swift
+++ Landmarks/Views/Badges/BadgeBackground.swift
@@ -15,9 +15,12 @@ struct BadgeBackground: View {
             Path { path in
                 var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
                 path.move(
                     to: CGPoint(
-                        x: width * 0.95,
+                        x: width * 0.95 + xOffset,
                         y: height * 0.20
                     )
                 )
@@ -25,18 +28,18 @@ struct BadgeBackground: View {
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
-                            x: width * segment.line.x,
+                            x: width * segment.line.x + xOffset,
                             y: height * segment.line.y
                         )
                     )
                     
                     path.addQuadCurve(
                         to: CGPoint(
-                            x: width * segment.curve.x,
+                            x: width * segment.curve.x + xOffset,
                             y: height * segment.curve.y
                         ),
                         control: CGPoint(
-                            x: width * segment.control.x,
+                            x: width * segment.control.x + xOffset,
                             y: height * segment.control.y
                         )
                     )
