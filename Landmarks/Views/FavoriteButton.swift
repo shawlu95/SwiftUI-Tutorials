5558dcf95c48767daf280f9d8b8f0b22a55e4cfb
Shaw Lu <shawlu95@gmail.com>
2023-08-20 09:20:11 +0800

    create FavoriteButton

---
+++ Landmarks/Views/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by main on 8/20/23.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
