a462548ed668faff1fd4421167106342cda5630a
Shaw Lu <shawlu95@gmail.com>
2023-08-20 10:20:54 +0800

    animate button state change

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -32,7 +32,10 @@ struct HikeView: View {
                         .labelStyle(.iconOnly)
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .animation(nil, value: showDetail) // no animation for rotation
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
+                        .animation(.spring(), value: showDetail)
                 }
             }
 
