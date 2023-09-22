d71637b62657046c8cadede467a06f5f4440507f
Shaw Lu <shawlu95@gmail.com>
2023-09-22 15:58:46 -0700

    add tab to select category view & list view

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 // conforms to the View protocol and describes the view’s content and layout.
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
