9f6468e2b0bf9db8adea91b4f744d30c47acabda
Shaw Lu <shawlu95@gmail.com>
2023-09-24 04:56:47 -0700

    group page related files

--- Landmarks/Views/PageView/FeatureCard.swift
+++ Landmarks/Views/PageView/FeatureCard.swift
@@ -23,14 +23,12 @@ struct FeatureCard: View {
 struct TextOverlay: View {
     var landmark: Landmark
 
-
     var gradient: LinearGradient {
         .linearGradient(
             Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]),
             startPoint: .bottom,
             endPoint: .center)
     }
-
 
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
