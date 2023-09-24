2d74ef48bc25c169307f2fd321736b7bcccb1e02
Shaw Lu <shawlu95@gmail.com>
2023-09-24 07:40:30 -0700

    add "show-side bar" in "View" drop-down menu

---
+++ Landmarks/Views/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by main on 2023/9/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        // This built-in command set includes the command for toggling the sidebar.
+        SidebarCommands()
+    }
+}
