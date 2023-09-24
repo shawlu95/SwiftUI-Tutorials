8d2cad293daa4c5ceb08bcd0a25f1d029d20980d
Shaw Lu <shawlu95@gmail.com>
2023-09-24 08:02:20 -0700

    create LandmarkSettings for Mac target

---
+++ Landmarks/Views/Landmarks/LandmarkSettings.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkSettings.swift
+//  Landmarks
+//
+//  Created by main on 2023/9/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkSettings()
+}
