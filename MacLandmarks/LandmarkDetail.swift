9bf9db2650c5ae57ab8fc69e45223ec75b0d65ca
Shaw Lu <shawlu95@gmail.com>
2023-09-24 06:53:42 -0700

    copy LandmarkDetail into mac (for starter)

---
+++ MacLandmarks/LandmarkDetail.swift
@@ -0,0 +1,75 @@
+//
+//  LandmarkDetail.swift
+//  MacLandmarks
+//
+//  Created by main on 2023/9/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            /*
+             When you specify only the height parameter, the
+             view automatically sizes to the width of its
+             content. In this case, MapView expands to fill
+             the available space.
+             */
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            // Each modifier returns a new view,
+            // so it’s common to chain multiple modifiers
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    Spacer()
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+
+                /*
+                 When you apply a modifier to a layout view
+                 like a stack, SwiftUI applies the modifier
+                 to all the elements contained in the group.
+                 */
+                HStack {
+                    Text(landmark.park)
+                    // A spacer expands to make its containing view use all of the space of its parent view,
+                    // instead of having its size defined only by its contents
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: ModelData().landmarks[1])
+}
