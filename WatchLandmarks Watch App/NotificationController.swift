64555dd695dab0f0544fb686ab89789b2f5407b6
Shaw Lu <shawlu95@gmail.com>
2023-09-24 08:41:33 -0700

    create NotificationController

---
+++ WatchLandmarks Watch App/NotificationController.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationController.swift
+//  WatchLandmarks Watch App
+//
+//  Created by main on 2023/9/24.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import WatchKit
+import SwiftUI
+import UserNotifications
+
+class NotificationController: WKUserNotificationHostingController<NotificationView> {
+    var landmark: Landmark?
+    var title: String?
+    var message: String?
+    
+    // use this key to extract the landmark index from the notification
+    let landmarkIndexKey = "landmarkIndex"
+    
+    override var body: NotificationView {
+        NotificationView(
+            title: title,
+            message: message,
+            landmark: landmark
+        )
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
