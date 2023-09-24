5a32d18a5aad9767f1e07ce9c000cb6c0f8ae68d
Shaw Lu <shawlu95@gmail.com>
2023-09-24 08:29:33 -0700

    display circle image

--- WatchLandmarks Watch App/LandmarkDetail.swift
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -16,10 +16,23 @@ struct LandmarkDetail: View {
         modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CircleImage(image: landmark.image.resizable())
+            .scaledToFill()
     }
 }
 
-#Preview {
-    LandmarkDetail(landmark: ModelData().landmarks[1])
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        let modelData = ModelData()
+        return Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 44mm")
+
+
+            LandmarkDetail(landmark: modelData.landmarks[1])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 5 - 40mm")
+        }
+    }
 }
