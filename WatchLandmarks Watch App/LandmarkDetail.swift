b620fa3d5f82641fd7a08e6773e39ff47d024069
Shaw Lu <shawlu95@gmail.com>
2023-09-24 08:34:04 -0700

    setup scroll view in landmark detail

--- WatchLandmarks Watch App/LandmarkDetail.swift
+++ WatchLandmarks Watch App/LandmarkDetail.swift
@@ -16,8 +16,37 @@ struct LandmarkDetail: View {
         modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
     var body: some View {
-        CircleImage(image: landmark.image.resizable())
-            .scaledToFill()
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
 }
 
